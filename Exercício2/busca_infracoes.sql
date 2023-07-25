f5efd2041e525179a0e7d9b8c1f64465c14840c2
caiokirst <caiokirst@gmail.com>
2023-07-24 23:23:35 -0300

    Update busca_infracoes.sql

--- Exercício2/busca_infracoes.sql
+++ Exercício2/busca_infracoes.sql
@@ -1,11 +1,12 @@
 SELECT
-    numero,
-    data,
-    hora,
-    equipamento,
-    velocidade_permitida,
-    velocidade_medida,
-    valida
-FROM infracao
+    i.remessa_id,
+    i.numero,
+    i.data,
+    i.hora,
+    i.equipamento,
+    i.velocidade_permitida,
+    i.velocidade_medida,
+    i.valida
+FROM infracao i 
 WHERE velocidade_medida >= (velocidade_permitida * 1.2) -- Velocidade medida igual ou acima de 20% da permitida
-ORDER BY data, hora;
+ORDER BY data, hora, remessa_id;
